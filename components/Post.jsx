29d5315948ed4bab34436f292b3b64bd9f1c4f71
Kurt Maglalang <kbm126@live.com>
2022-10-31 20:56:18 -0700

    setup basic post card

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Link from "next/link";
 
-const Post = () => {
-  return <div>Post</div>;
+const Post = ({ post }) => {
+  return (
+    <div className="rounded-lg bg-gray-200 shadow-lg lg:p-4">
+      <div>
+        <Link href={"#"}>{post.title}</Link>
+      </div>
+      <div>image</div>
+      <div>{post.excerpt}</div>
+    </div>
+  );
 };
 
 export default Post;
