425030d3d4d02ced1908e4bdda725fdba4be69b3
Kurt Maglalang <kbm126@live.com>
2022-10-31 20:26:03 -0700

    reference to background image creator

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -16,6 +16,12 @@ const Footer = () => {
           <Link href={"#"}>placeholder</Link>
         </div>
       </div>
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        <a href="https://www.freepik.com/free-vector/white-background-with-triangle-patterns_4403214.htm#query=abstract%20background&position=37&from_view=keyword">
+          Image by starline
+        </a>{" "}
+        on Freepik
+      </span>
     </footer>
   );
 };
