c61b8381a7393089f41d5322a43c8d69d86fc17b
Kurt Maglalang <kbm126@live.com>
2022-10-31 20:09:10 -0700

    setup temp header

---
+++ components/Header.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="border-b w-full inline-block py-4 border-gray-400">
+        <div className="md:float-left block">
+          <Link href={"/"}>
+            <span className="font-bold text-3xl text-slate-600">Blog</span>
+          </Link>
+        </div>
+        <div className="md:float-right text-blue-400 mt-2">placeholder</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
