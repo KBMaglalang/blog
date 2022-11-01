4cd50b26f86328f7699ab2763a462e9405a4518c
Kurt Maglalang <kbm126@live.com>
2022-11-01 13:55:54 -0700

    add date when post was created

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Link from "next/link";
+import moment from "moment/moment";
 
 const Post = ({ post }) => {
-  console.log("🚀 ~ file: Post.jsx ~ line 5 ~ Post ~ post", post);
   return (
     <Link href={"#"}>
       <div className="rounded-lg bg-gray-200 shadow-lg lg:p-4 mb-8">
@@ -10,6 +10,9 @@ const Post = ({ post }) => {
           <span>{post.node.title}</span>
         </div>
         <div>image</div>
+        <div className="py-4 text-slate-400">
+          <span>{moment(post.node.createdAt).format("MMM DD, YYYY")}</span>
+        </div>
         <div className="mt-4">{post.node.excerpt}</div>
       </div>
     </Link>
