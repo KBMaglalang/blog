4321215ad3222740cad742b6fc23a6f1b8f0b6b2
Kurt Maglalang <kbm126@live.com>
2022-10-31 19:52:14 -0700

    update to JSX files

---
+++ pages/index.jsx
@@ -0,0 +1,10 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
+
+{
+  /* <a href="https://www.freepik.com/free-vector/white-background-with-triangle-patterns_4403214.htm#query=abstract%20background&position=37&from_view=keyword">Image by starline</a> on Freepik */
+}
