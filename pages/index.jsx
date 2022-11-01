dae33220a77fb9d8116bb76a5f2d3a94b0ed6ff8
Kurt Maglalang <kbm126@live.com>
2022-10-31 20:56:10 -0700

    setup test data

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,18 @@
 import Head from "next/head";
 
+import { Post } from "../components";
+
+const data = [
+  {
+    title: "post-1",
+    excerpt: "short description of post-1",
+  },
+  {
+    title: "post-2",
+    excerpt: "short description of post-2",
+  },
+];
+
 export default function Home() {
   return (
     <div className="container mx-auto px-10 mb-8">
@@ -12,7 +25,11 @@ export default function Home() {
           <div className="lg:col-span-2 col-span-1">
             <div className="relative top-8 lg:sticky">side bar</div>
           </div>
-          <div className="lg:col-span-8 col-span-1">content</div>
+          <div className="lg:col-span-8 col-span-1">
+            {data.map((post, i) => (
+              <Post post={post} key={i} />
+            ))}
+          </div>
         </div>
       </main>
     </div>
