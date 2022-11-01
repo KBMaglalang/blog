080b5f8ef81d8f999a6dcb3b4f4b82c5c7fec4d4
Kurt Maglalang <kbm126@live.com>
2022-11-01 14:01:40 -0700

    setup image in post

--- components/Post.jsx
+++ components/Post.jsx
@@ -6,10 +6,16 @@ const Post = ({ post }) => {
   return (
     <Link href={"#"}>
       <div className="rounded-lg bg-gray-200 shadow-lg lg:p-4 mb-8">
-        <div className="text-center mb-4 font-semibold text-2xl">
+        <div className="text-center mb-4 font-semibold text-2xl pt-4">
           <span>{post.node.title}</span>
         </div>
-        <div>image</div>
+        <div className="relative">
+          <img
+            src={post.node.featuredImage.url}
+            alt={post.node.title}
+            className="w-full shadow-lg rounded-lg"
+          />
+        </div>
         <div className="py-4 text-slate-400">
           <span>{moment(post.node.createdAt).format("MMM DD, YYYY")}</span>
         </div>
