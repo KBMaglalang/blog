49c8c0a0213e70b4b1bc011e2178392a11230165
Kurt Maglalang <kbm126@live.com>
2022-11-01 18:06:51 -0700

    setup post page

---
+++ pages/post/[slug].jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import moment from "moment/moment";
+import { RichText } from "@graphcms/rich-text-react-renderer";
+
+// queries
+import { getPosts, getPostData } from "../../lib/queries";
+
+const Page = ({ post }) => {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className="rounded-lg bg-gray-200 shadow-lg lg:p-4 mb-8">
+        <div>
+          <img
+            src={post.featuredImage.url}
+            alt={post.title}
+            className="h-full w-full shadow-lg rounded-lg"
+          />
+        </div>
+        <div className="px-4 lg:px-0">
+          <div className="py-4 text-slate-400">
+            <span>{moment(post.createdAt).format("MMM DD, YYYY")}</span>
+          </div>
+          <h1 className="mb-8 text-3xl font-semibold">{post.title}</h1>
+          <RichText content={post.content.raw} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
+
+export const getStaticPaths = async () => {
+  const posts = await getPosts();
+  console.log(
+    "🚀 ~ file: [slug].jsx ~ line 35 ~ getStaticPaths ~ posts",
+    posts
+  );
+  return {
+    paths: posts.map(({ node: { slug } }) => ({ params: { slug } })),
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const data = await getPostData(params.slug);
+
+  return {
+    props: { post: data },
+    // Next.js will attempt to re-generate the page:
+    // - When a request comes in
+    // - At most once every 10 seconds
+    revalidate: 10, // In seconds
+  };
+};
