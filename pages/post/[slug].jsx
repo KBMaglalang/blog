3b2c7f72a06c5c5dd988ffe90234b0ae949205e5
Kurt Maglalang <kbm126@live.com>
2022-11-02 15:33:30 -0700

    apply gap only on large devices

--- pages/post/[slug].jsx
+++ pages/post/[slug].jsx
@@ -11,7 +11,7 @@ import { getPosts, getPostData } from "../../lib/queries";
 const Page = ({ post }) => {
   return (
     <div className="container mx-auto px-10 mb-8">
-      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+      <div className="grid grid-cols-1 lg:grid-cols-12 lg:gap-12">
         <div className="lg:col-span-4 col-span-1">
           <div className="top-8 lg:sticky">
             <PostsHistory />
