74f989a36cc03ca9e3142bb6c584a1651736be63
Kurt Maglalang <kbm126@live.com>
2022-11-02 14:46:08 -0700

    remove extra div

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,14 +12,13 @@ const Footer = () => {
           </Link>
           . All Rights Reserved.
         </span>
-        <div className="md:float-right text-blue-400 ">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-            <a href="https://www.freepik.com/free-vector/white-background-with-triangle-patterns_4403214.htm#query=abstract%20background&position=37&from_view=keyword">
-              Background Image by starline
-            </a>{" "}
-            on Freepik
-          </span>
-        </div>
+
+        <span className="md:float-right text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          <a href="https://www.freepik.com/free-vector/white-background-with-triangle-patterns_4403214.htm#query=abstract%20background&position=37&from_view=keyword">
+            Background Image by starline
+          </a>{" "}
+          on Freepik
+        </span>
       </div>
     </footer>
   );
