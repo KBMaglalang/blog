79aac208e6ecbdbfca02ec44c11c5a3b1d0fa784
Kurt Maglalang <kbm126@live.com>
2022-11-01 23:53:11 -0700

    create api route to submit comment

---
+++ pages/api/graphQL.js
@@ -0,0 +1,47 @@
+/** *************************************************************
+ * Any file inside the folder pages/api is mapped to /api/* and  *
+ * will be treated as an API endpoint instead of a page.         *
+ *************************************************************** */
+
+import { GraphQLClient, gql } from "graphql-request";
+
+const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
+const graphcmsToken = process.env.GRAPHCMS_TOKEN;
+
+export default async function comments(req, res) {
+  // const { name, email, slug, comment } = req.body;
+
+  const graphQLClient = new GraphQLClient(graphqlAPI, {
+    headers: {
+      authorization: `Bearer ${graphcmsToken}`,
+    },
+  });
+
+  const query = gql`
+    mutation CreateComment(
+      $name: String!
+      $email: String!
+      $comment: String!
+      $slug: String!
+    ) {
+      createComment(
+        data: {
+          name: $name
+          email: $email
+          comment: $comment
+          post: { connect: { slug: $slug } }
+        }
+      ) {
+        id
+      }
+    }
+  `;
+
+  try {
+    const result = await graphQLClient.request(query, req.body);
+    return res.status(200).send(result);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send(error);
+  }
+}
