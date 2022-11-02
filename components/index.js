692c2a557f14bfacdd2e9cd821cdb29a4cfe1b8b
Kurt Maglalang <kbm126@live.com>
2022-11-01 22:50:18 -0700

    export components

--- components/index.js
+++ components/index.js
@@ -2,3 +2,5 @@ export { default as Header } from "./Header";
 export { default as Footer } from "./Footer";
 export { default as Post } from "./Post";
 export { default as PostsHistory } from "./PostsHistory";
+export { default as Comments } from "./Comments";
+export { default as CommentsForm } from "./CommentsForm";
